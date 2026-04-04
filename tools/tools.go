c03ab1b5cbe54120263b48d3d8854659a64c4288
Your Name <you@example.com>
2026-04-04 09:22:39 -0500

    build(tools): pin CI toolchain versions natively via tools.go

---
+++ tools/tools.go
@@ -0,0 +1,14 @@
+//go:build tools
+// +build tools
+
+package tools
+
+import (
+	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
+	_ "github.com/jgautheron/goconst/cmd/goconst"
+	_ "github.com/mibk/dupl"
+	_ "github.com/uudashr/gocognit/cmd/gocognit"
+	_ "github.com/zricethezav/gitleaks/v8"
+	_ "golang.org/x/tools/go/analysis/passes/fieldalignment/cmd/fieldalignment"
+	_ "golang.org/x/vuln/cmd/govulncheck"
+)
